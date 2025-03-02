7f6b45e5c02ba8b1a5ffbed5efd9369147640ead
deeleepppp <kr.dilipsinghh@gmail.com>
2025-03-02 15:32:15 +0530

    add lazy Loading

--- src/utils/Router.jsx
+++ src/utils/Router.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { lazy, Suspense ,useState } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import HumanVerification from "../components/humanVerification/HumanVerification";
-import Combination from "../components/Combination";
+// import Combination from "../components/Combination";
+const CombinationLoading = lazy(() => import("../components/Combination"))
 
 const Router = () => {
   const [isToggled, setIsToggled] = useState(false);
@@ -31,7 +32,11 @@ const Router = () => {
           path="/"
           element={<HumanVerification isToggled={isToggled} setIsToggled={handleToggle} />}
         />
-        <Route path="/page" element={<Combination />} />
+        <Route path="/page" element={ <Suspense fallback={<div className="fixed inset-0 flex justify-center items-center bg-black">
+          <img src="/images/loader.gif" className="h-40 w-40" alt="" />
+        </div>}>
+              <CombinationLoading />
+            </Suspense>} />
       </Routes>
     </>
   );
